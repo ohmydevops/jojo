a11cf498a5db14d2243b77a571f6a0d69bfa3872
ohmydevops <ohmydevops@gmail.com>
2021-11-23 16:39:27 +0330

    Support file serving

--- main.php
+++ main.php
@@ -1,12 +1,29 @@
 <?php
+
 declare(strict_types=1);
+
+use JetBrains\PhpStorm\NoReturn;
 
 $interface = '0.0.0.0';
 $port = 8000;
-$defaults = array(
-    'Content-Type' => 'text/html',
-    'Server' => 'PHP '. phpversion()
-);
+
+// Properly configuring server MIME types:
+// https://developer.mozilla.org/en-US/docs/Learn/Server-side/Configuring_server_MIME_types
+$contentTypes = [
+    'html' => 'text/html;charset=utf-8',
+    'css' => 'text/css',
+    'js' => 'text/javascript'
+];
+
+$defaultHeaders = [
+    'Server' => 'PHP ' . phpversion()
+];
+
+function fileMimeDetector(string $requestedFile, array $contentTypes): string
+{
+    $fileExtension = pathinfo($requestedFile, PATHINFO_EXTENSION);
+    return $contentTypes[$fileExtension];
+}
 
 $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 if ($sock === false) {
@@ -15,7 +32,7 @@ if ($sock === false) {
 }
 
 if (!socket_set_option($sock, SOL_SOCKET, SO_REUSEADDR, 1)) {
-    echo 'Unable to set option on socket: '. socket_strerror(socket_last_error()) . PHP_EOL;
+    echo 'Unable to set option on socket: ' . socket_strerror(socket_last_error()) . PHP_EOL;
 }
 
 $isBind = socket_bind($sock, $interface, $port);
@@ -29,37 +46,67 @@ if ($isListen === false) {
     exit();
 }
 
-echo "Server is running on $interface:$port" . PHP_EOL;
-
-
-$app = function($request) {
-    $htmlBody = file_get_contents('index.html');
-    return array(
-        '200 OK',
-        array('Content-Type' => 'text/html;charset=utf-8'),
-        $htmlBody
-    );
+$handleFileResponse = function (string $requestedFile) use ($contentTypes) {
+    $body = file_get_contents($requestedFile);
+    return ['200 OK', ['Content-Type' => fileMimeDetector($requestedFile, $contentTypes)], $body];
 };
+
+$handleNotFoundResponse = function () {
+    return [
+        '404 Not Found',
+        ['Content-Type' => 'text/html'],
+        "<html lang='fa' dir='rtl'><head><title>یافت نشد</title></head><body><h3>همچین فایلی وجود ندارد</h3></body></html>"
+    ];
+};
+
+
+echo "Server is running on $interface:$port" . PHP_EOL;
 
 while ($conn = socket_accept($sock)) {
     $request = '';
     while (!str_ends_with($request, "\r\n\r\n")) {
         $request .= socket_read($conn, 1024);
     }
-    list($code, $headers, $body) = $app($request);
-    $headers += $defaults;
-    if (!isset($headers['Content-Length'])) {
-        $headers['Content-Length'] = strlen($body);
+    $parsedData = explode("\r", $request);
+    $requestedFile = basename(explode(" ", $parsedData[0])[1]);
+    if (file_exists($requestedFile) === false) {
+        list($code, $headers, $body) = $handleNotFoundResponse();
+        $headers += $defaultHeaders;
+        if (!isset($headers['Content-Length'])) {
+            $headers['Content-Length'] = strlen($body);
+        }
+        $header = '';
+        foreach ($headers as $k => $v) {
+            $header .= $k . ': ' . $v . "\r\n";
+        }
+        socket_write(
+            $conn,
+            implode("\r\n", array(
+                'HTTP/1.1 ' . $code,
+                $header,
+                $body
+            ))
+        );
+        socket_close($conn);
+    } else {
+        list($code, $headers, $body) = $handleFileResponse($requestedFile);
+        $headers += $defaultHeaders;
+        if (!isset($headers['Content-Length'])) {
+            $headers['Content-Length'] = strlen($body);
+        }
+        $header = '';
+        foreach ($headers as $k => $v) {
+            $header .= $k . ': ' . $v . "\r\n";
+        }
+        socket_write(
+            $conn,
+            implode("\r\n", array(
+                'HTTP/1.1 ' . $code,
+                $header,
+                $body
+            ))
+        );
+        socket_close($conn);
     }
-    $header = '';
-    foreach ($headers as $k => $v) {
-        $header .= $k.': '.$v."\r\n";
-    }
-    socket_write($conn, implode("\r\n", array(
-        'HTTP/1.1 '.$code,
-        $header,
-        $body
-    )));
-    socket_close($conn);
+    echo $code . ' ' . $parsedData[0] . PHP_EOL;
 }
-socket_close($sock);
