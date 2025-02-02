57e63c82982973e8aaa26230d177a427bceccfd6
ohmydevops <amir@tofisa.com>
2025-02-02 14:31:09 +0330

    Update concurrency test script

--- curl_samples.sh
+++ curl_samples.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+#########
+# How to run: time ./curl_samples.sh 
+#########
 
 for ((counter=1; counter<=1000; counter++))
 do
-  time curl --silent -X GET "http://localhost:8000/" &
+  curl --silent --output /dev/null --write-out "$counter - %{http_code}\n" "http://localhost:8000/" &
 done
+
+wait
